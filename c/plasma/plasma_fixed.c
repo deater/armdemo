276971bfd72fadd71b91ef0b9b564855626ef596
Vince Weaver <vincent.weaver@maine.edu>
2025-01-14 01:41:58 -0500

    plasma: more fixed point conversion

--- c/plasma/plasma_fixed.c
+++ c/plasma/plasma_fixed.c
@@ -1,12 +1,75 @@
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <math.h>
 
 char output[65536];
 
-double cos_table[80],sin_table[24];
+int32_t double_to_fixed(double in) {
+
+	int32_t x=0;
+
+	x=round(in * (1 << 16));
+
+	return x;
+}
+
+int32_t fixed_mul(int32_t x, int32_t y) {
+
+	int64_t result,x1,y1;
+
+	x1=x;
+	y1=y;
+
+	result=((x1*y1)>>16);
+
+	return result;
+}
+
+int32_t our_sin(int32_t x) {
+
+	int32_t result;
+	int32_t x3,x5;
+	int32_t x3t,x5t;
+
+	// sin(x) ~= x - (x^3)/3!  + (x^5)/5! - (x^7)/7!
+
+	x3=fixed_mul(x,x);
+	x3=fixed_mul(x3,x);
+	x3t=fixed_mul(x3,double_to_fixed(1.0/6.0));
+
+	x5=fixed_mul(x3,x);
+	x5=fixed_mul(x5,x);
+	x5t=fixed_mul(x5,double_to_fixed(1.0/120.0));
+
+	result=x-x3t+x5t;
+
+	return result;
+}
+
+// cos
+//      1    0
+//
+//
+// sin   0   1
+
+// cos=sine shifted left by pi/2
+
+//        0  pi/2  pi
+// sin    0   1    0
+// cos    1   0
+int32_t our_cos(int32_t x) {
+
+	int32_t offset;
+
+	offset=double_to_fixed(1.57);
+
+	return our_sin(offset-x);
+}
+
+
+#if 0
 
 int setup_table(void) {
 
@@ -24,24 +87,29 @@ int setup_table(void) {
 	return 0;
 }
 
+#endif
+
 int main(int argc, char **argv) {
 
 	int length;
-	int x,y;
-	double c,t=0;
+	int32_t x,y,xx,yy;
+	int32_t c,t=0;
 	int o;
 
 	char string[1024];
-
-	setup_table();
 
 	while(1) {
 		strcpy(output,"\x1b[1;1H");
 
 		for(y=0;y<24;y++) {
 			for(x=0;x<80;x++) {
-				c=cos_table[x]+sin_table[y]+t;
-				o=(c*64.0);
+				xx=(x<<16)>>7;
+				yy=(y<<16)>>7;
+				c=our_cos(xx)+ // cos
+					our_sin(yy)+t;
+				//o=(c*64.0);	// <<5 then >> 16
+
+				o=((c<<5)>>16);
 
 				sprintf(string,
 					"\x1b[38;2;%d;%d;%dm%c",
@@ -59,7 +127,7 @@ int main(int argc, char **argv) {
 		write(STDOUT_FILENO,output,length);
 
 		usleep(30000);
-		t=t+1.0/200.0;
+		t=t+double_to_fixed(1.0/200.0);
 
 	}
 	return 0;
