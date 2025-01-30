c3dfac63a52f390b69f89ab49355264117673c88
Vince Weaver <vincent.weaver@maine.edu>
2025-01-30 17:18:12 -0500

    plasma: more work on assembly

--- linux/plasma/plasma.s
+++ linux/plasma/plasma.s
@@ -14,9 +14,49 @@ YHEIGHT	=	24
 .equ STDERR,2
 
 @ register allocation
-
+@
+@ r0, r1 = arguments
+@ r6 = output pointer
+@ r7 = temp
+@ r8 = t
         .globl _start
 _start:
+	ldr	r6,=out_buffer
+	mov	r8,#0
+
+plasma_loop:					@ while(1) {
+	ldr	r0,=move_to_start		@ strcpy(output,"\x1b[1;1H");
+	bl	strcat
+
+.if 0
+		for(y=0;y<24;y++) {
+			for(x=0;x<80;x++) {
+				xx=x<<9;	// xx=(x<<16)>>7;
+				yy=y<<9;	// yy=(y<<16)>>7;
+				c=our_cos(xx+t)+ // cos
+					our_sin(yy)+t*2;
+				//o=(c*64.0);	// <<5 then >> 16
+
+				o=c>>11;	// o=((c<<5)>>16);
+
+				int cc=c>>8;
+
+				sprintf(string,
+					"\x1b[38;2;%d;%d;%dm%c",
+					(cc&0x3f)*4,
+					((cc+32)&0x3f)*4,
+					((cc+48)&0x3f)*4,
+					(o&0x3f)+' ');
+
+				strcat(output,string);
+			}
+			strcat(output,"\n");
+
+		}
+.endif
+
+
+.if 0
 
 forever_loop:
 	movs	r2,#0				@ clear strcat count
@@ -51,6 +91,14 @@ xloop:
 	cmp	r4,#YHEIGHT
 	bne	yloop
 
+.endif
+
+
+	@ length=strlen(output);
+	@ write(STDOUT_FILENO,output,length);
+
+	ldr	r1,=out_buffer
+	sub	r2,r6,r1
 
 	@================================
 	@ write stdout
@@ -59,27 +107,71 @@ xloop:
 	@ len in r2
 write_stdout:
 	movs	r0,#STDOUT
-	mov	r1,r6
 	movs	r7,#SYSCALL_WRITE
 	swi	#0
 
+@	usleep(30000);
+@	t=t+0x148; // (1/200)
 
-	b	forever_loop
+	add	r8,r8,#0x148
+
+
+	b	plasma_loop
 
 	@================================
 	@ strcat
 	@================================
-	@ len in r0
-	@ string in r1
+	@ string in r0
 strcat:
 
 strcat_loop:
-	ldrb	r7,[r1],#1		@ load input, then increment r1 after
-	strb	r7,[r6,r2]		@ store to out_buffer+r2
-	adds	r2,r2,#1
-	subs	r0,r0,#1
-	bne	strcat_loop
+	ldrsb	r7,[r0],#1		@ load input, then increment r1 after
+	beq	strcat_done
+	strb	r7,[r6],#1		@ store to out_buffer+r2
+	b	strcat_loop
+strcat_done:
 	bx	lr			@ return
+
+
+
+fixed_mul:
+@	result=((x1*y1)>>16);		@
+
+
+
+our_cos:
+@static int32_t our_cos(int32_t x) {
+@
+@	int32_t offset;
+
+@	offset=0x19220;	/* pi/2 */
+
+@//	offset=double_to_fixed(1.57);
+
+@	return our_sin(offset-x);
+
+
+
+our_sin:
+@	int32_t result;
+@	int32_t x2,x3,x5;
+@	int32_t x3t,x5t;
+
+@	// sin(x) ~= x - (x^3)/3!  + (x^5)/5! - (x^7)/7!
+
+@	x2=fixed_mul(x,x);
+@	x3=fixed_mul(x2,x);
+@	x3t=fixed_mul(x3,0x2AAB);	// double_to_fixed(1.0/6.0));
+
+@	x5=fixed_mul(x3,x2);
+@	x5t=fixed_mul(x5,0x222);	// double_to_fixed(1.0/120.0));
+
+@	result=x-x3t+x5t;
+
+@	return result;
+
+
+
 
 .data
 
@@ -87,7 +179,8 @@ color_string:
 	.ascii	"\033[40m "		@ 0
 linefeed:
 	.ascii	"\n"			@ 6
-clear_screen:
-	.ascii "\033[2J"		@ 4 long
+move_to_start:
+	.ascii "\033[1;1H\000"		@ 6
+
 .bss
 .lcomm	out_buffer, 65536
