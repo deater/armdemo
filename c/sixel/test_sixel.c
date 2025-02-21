7d842c82eda42b52a0cf2c1af43660350a49282d
Vince Weaver <vincent.weaver@maine.edu>
2025-02-21 16:22:37 -0500

    sixel: look into sixel support

---
+++ c/sixel/test_sixel.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+	printf("\033Pq\n");
+	printf("#0;2;0;0;0#1;2;100;100;0#2;2;0;100;0\n");
+	printf("#1~~@@vv@@~~@@~~$\n");
+	printf("#2??}}GG}}??}}??-\n");
+	printf("#1!14@\n");
+	printf("\033\\\n");
+
+	return 0;
+}
