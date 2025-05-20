4a2575b3e05c57ff2bfda9acaba2e63a5ec46e55
Vince Weaver <vince@deater.net>
2025-05-20 00:22:52 -0400

    starpath: more compact

--- c/sixel/star_path_sixel_compact.c
+++ c/sixel/star_path_sixel_compact.c
@@ -1,29 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 
-static unsigned char framebuffer[320][200];
-
-static int check_color(int y, int c) {
-	int x,row;
-
-	for(row=0;row<6;row++) {
-		for(x=0;x<320;x++) {
-			if (framebuffer[x][y*6+row]==c) return 1;
-		}
-	}
-
-	return 0;
-
-}
-
-
-
-
-
 int main(int argc, char **argv) {
 
-	int frame=0,color,depth,x,y,yprime,xprime;
-	int temp;
+unsigned char fb[320][200];
+int f=0,color,depth,x,y,yprime,xprime,temp;
 
 for(;;){
 for(x=0;x<256;x++) {
@@ -44,53 +25,38 @@ else {// multiply X by current depth (into AH)
 xprime=temp*depth;// OR for geometry and texture pattern
 temp=((xprime)|(yprime))>>8;	// get (current depth) + (current frame)
 				// mask geometry/texture by time shifted depth
-color=(temp&(depth+frame));// (increment depth by one)
+color=(temp&(depth+f));// (increment depth by one)
 depth++; // ... to create "gaps"
 if ((color&0x10)==0) goto L;// if ray did not hit, repeat pixel loop
 color-=16;
 }
-framebuffer[x][y]=color;
+fb[x][y]=color;
 }
 }
 usleep(10000);
-frame++;		// increment frame counter
+f++;		// increment frame counter
 puts("\n\033[1;1H");
 
-int x,y,c,row,value;
-int s=0;
+int c,row,v,s=0;
 
 /* start sixel */
 puts("\033Pq\n");
 /* setup palette */
 char*a="#%i;2;%d;%d;%d\n";for(;s<96;s+=6)printf(a,s/6,s,s,s),
 printf(a,s/6+16,s>25?s>72?97-s:25:s,s>72?25:s>42?s-48:0,s<48?s<24?25:48-s:0);
-
-/* print a row 34*6=204 33*6=198*/
+ /* print a row 34*6=204 33*6=198*/
 char b[]="-\n#%d";
-for(y=0;y<200/6;y++) {
-b[0]='-';
-	for(c=0;c<32;c++) { // MAXCOLORS
-			if (check_color(y,c)) {
-				printf(b,c);
-				b[0]='$';
-				for(x=0;x<320;x++) {
-					value=0;
-					for(row=5;row>=0;row--) {
-						value<<=1;
-						if (framebuffer[x][y*6+row]==c) {
-							value|=1;
-						}
-					}
-					printf("%c",value+'?');
-				}
-			}
+for(y=0;y<200/6;y++) { *b='-'; for(c=0;c<32;c++) {
+for(row=0;row<6;row++) for(x=0;x<256;x++)
+if (fb[x][y*6+row]==c) break;
+	if (row==6) {
+		printf(b,c);*b='$';
+		for(x=0;x<256;x++) {
+			v=0;
+			for(row=5;row>=0;row--) v=v<<1|(fb[x][y*6+row]==c);
+			putchar(v+'?');
 		}
-
 	}
-	/* finished */
-	printf("\n\033\\\n");
-
-
-}
-return 0;
-}
+} // c
+} // y
+printf("\n\033\\\n");}return 0;}
