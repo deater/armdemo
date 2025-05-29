f11adb7abe1004bdef063eb64c670ef53868df59
Vince Weaver <vincent.weaver@maine.edu>
2025-05-29 16:02:50 -0400

    music: work on tiny player

---
+++ music/parse2.c
@@ -0,0 +1,191 @@
+/* This parses the cvs output from TIAtracker */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "common.h"
+
+static int debug=1;
+
+static int min_note=64,max_note=0;
+
+static int convert_note(char *note) {
+
+	int octave=0,raw,sharp=0,result;
+
+	if (debug) fprintf(stderr,"Converting %s -- ",note);
+
+	if (note[2]=='-') {
+//		printf("-------------------\n");
+		return '~';
+	}
+
+
+	switch(note[0]) {
+		case 'C': raw=0; break;
+		case 'D': raw=2; break;
+		case 'E': raw=4; break;
+		case 'F': raw=5; break;
+		case 'G': raw=7; break;
+		case 'A': raw=9; break;
+		case 'B': raw=11; break;
+
+		default:
+			fprintf(stderr,"ERROR! %x\n",note[0]);
+			return '~';
+	}
+	if (note[1]=='#') sharp++;
+
+	octave=(note[2]-'0');
+
+	result=(raw+sharp)+((octave-OCTAVE_START)*12);
+
+	if (result>max_note) max_note=result;
+	if (result<min_note) min_note=result;
+
+	fprintf(stderr,"Got %d (%d %d %d)\n",result+OFFSET,raw,sharp,octave);
+
+	return result+OFFSET;
+
+
+}
+
+static int numlens=0;
+static int len_list[1024];
+
+
+
+static void add_len(int len) {
+	int i;
+
+	for(i=0;i<numlens;i++) {
+		if (len==len_list[i]) break;
+	}
+	if (i==numlens) {
+		len_list[i]=len;
+		numlens++;
+	}
+
+}
+
+int main(int argc, char **argv) {
+
+	char string[BUFSIZ];
+	char *result, *token;
+	char *saveptr1;
+	int which,line=0;
+	char note[4];
+
+	int last='~',len=0;
+
+	int output_ptr=0;
+	int output_notes[1024];
+	int output_lengths[1024];
+	int i;
+
+	int which_col=5;
+
+	if (argc>1) which_col=atoi(argv[1]);
+
+	/* skip header */
+	result=fgets(string,BUFSIZ,stdin);
+	if (result==NULL) {
+		fprintf(stderr,"ERROR!  empty file!\n");
+		return -1;
+	}
+
+
+	while(1) {
+
+		result=fgets(string,BUFSIZ,stdin);
+		if (result==NULL) break;
+
+		if (line%64==0) {
+			output_notes[output_ptr]=last;
+			add_len(len);
+			output_lengths[output_ptr]=len+OFFSET;
+			output_ptr++;
+			last='~';
+			len=0;
+
+			output_notes[output_ptr]='0';
+			output_lengths[output_ptr]='0';
+			output_ptr++;
+		}
+
+
+
+
+
+		which=0;
+		while(1) {
+			if (which==0) {
+				token = strtok_r(string,",", &saveptr1);
+			}
+			else {
+				token = strtok_r(NULL,",", &saveptr1);
+			}
+			if (token == NULL) break;
+
+			if (which==which_col) {
+				note[0]=token[6];
+				note[1]=token[7];
+				note[2]=token[8];
+				note[3]=0;
+				len++;
+
+				if ((note[0]!=' ') || (note[2]=='-')) {
+//					if (debug) {
+//						print_note(last);
+//					}
+					output_notes[output_ptr]=last;
+					add_len(len);
+					output_lengths[output_ptr]=len+OFFSET;
+					output_ptr++;
+					last=convert_note(note);
+					len=0;
+				}
+			}
+			which++;
+		}
+		line++;
+
+	}
+
+	printf("unsigned char notes[]=\"\n\"");
+	for(i=0;i<output_ptr;i++) {
+		if (output_notes[i]=='0') {
+			printf("\"\n\"");
+		}
+		else if (output_notes[i]=='\\') {
+			putchar('\\');
+			putchar('\\');
+		}
+		else {
+			putchar(output_notes[i]);
+		}
+	}
+	printf("\";\n");
+
+	printf("unsigned char lens[]=\"\n\"");
+	for(i=0;i<output_ptr;i++) {
+		if (output_notes[i]=='0') {
+			printf("\"\n\"");
+		}
+		else {
+			putchar(output_lengths[i]);
+		}
+	}
+	printf("\";\n");
+
+	printf("/* Found lens: ");
+	for(i=0;i<numlens;i++) {
+		printf("%d ",len_list[i]);
+	}
+	printf("*/\n");
+
+	printf("/* Min: %d, Max: %d */\n",min_note,max_note);
+
+	return 0;
+}
--- music/player2.c
+++ music/player2.c
@@ -5,10 +5,12 @@
 #include "common.h"
 
 #include "music_track1.h"
+#include "music_track2.h"
 
 /* music size: 800 lines, each 1/16 note, 1000 samples each = 800k */
 
 #define BUFFER_SIZE 1*1024*1024
+#define SAMPLE_FREQ	8000
 
 static int frequencies[64];
 
@@ -29,13 +31,10 @@ static int generate_frequencies(void) {
 }
 
 
-#define SAMPLE_FREQ	8000
+
 
 unsigned char buffer[BUFFER_SIZE];
 int offset=0;
-
-
-
 
 static void play_note(int note, int length) {
 
@@ -79,6 +78,59 @@ static void play_note(int note, int length) {
 	}
 }
 
+
+static void add_note(int note, int length) {
+
+	int on, d,j, volume;
+
+	/* FIXME: maybe should use floating/fixed point to get */
+	/*        more accurate frequencies.  Mostly an issue */
+	/*	  with higher notes */
+
+	if (note>64) {
+		on=0;
+	}
+	else {
+		// 8000, 440   8000/440/2=
+		on=SAMPLE_FREQ/frequencies[note];
+	}
+
+	// 120bpm quaternote = 0.5 s */
+	// assume 120bpm = 8000/(120/60) = 4000 long, sixteenth = 1000 long */
+	d=length*(SAMPLE_FREQ/8);
+
+//	printf("Note:%d duration %d\n",note,d);
+	for(j=0;j<d;j++) {
+		// add a bit of an envelope
+
+		if (j>(d/2)) volume=0x40;
+		else volume=0x7f;
+
+		if (on==0) {
+			if (offset<BUFFER_SIZE) {
+				buffer[offset]=(buffer[offset]+0)/2;
+				offset++;
+			}
+		}
+		else {
+
+			if (j%on<on/2) {
+				if (offset<BUFFER_SIZE) {
+					buffer[offset]=(buffer[offset]+volume)/2;
+					offset++;
+				}
+			}
+			else {
+				if (offset<BUFFER_SIZE) {
+					buffer[offset]=(buffer[offset]+0)/2;
+					offset++;
+				}
+			}
+		}
+	}
+}
+
+
 int main(int argc, char **argv) {
 
 	FILE *pipe;
@@ -88,6 +140,7 @@ int main(int argc, char **argv) {
 
 	generate_frequencies();
 
+	/* track 1 */
 	i=0;
 	while(1) {
 		note=track1_notes[i];
@@ -102,6 +155,24 @@ int main(int argc, char **argv) {
 
 		i++;
 	}
+
+	/* track 2 */
+	offset=0;
+	i=0;
+	while(1) {
+		note=track2_notes[i];
+		length=track2_lens[i];
+		if (note==0) break;
+
+		printf("Note %d: ",i);
+		print_note(note);
+		printf("length %d\n",length-OFFSET);
+
+		add_note(note-OFFSET,length-OFFSET);
+
+		i++;
+	}
+
 
 	pipe=popen("/usr/bin/aplay","w");
 	if (pipe==NULL) {
